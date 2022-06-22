3dbc244298ea24269eb617d5c022b42e244a083e
Gilang Ramadhan <gilangramadhannugraha@outlook.co.id>
2022-06-22 15:51:34 +0700

    initial configuration project

---
+++ lib/common/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+//Smoth Colors
+const kYellow = Color(0xFFFAF0DA);
+const kYellowLight = Color(0xFFFFF7EC);
+const kYellowDark = Color(0xFFEBBB7F);
+
+const kRed = Color(0xFFFBE4e6);
+const kRedLight = Color(0xFFFCF0F0);
+const kRedDark = Color(0xFFF08A8E);
+
+const kBlue = Color(0xFFE1EDFC);
+const kBlueLight = Color(0xFFEDF4FE);
+const kBlueDark = Color(0xFFC0D3F8);
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(INoteApp());
+
+class INoteApp extends StatelessWidget {
+  const INoteApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'INote',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:,
+    );
+  }
+}
+
